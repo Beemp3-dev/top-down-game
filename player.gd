51b47fbb5af6cc07b195a6220690fb0218d24005
Beemp3 <thecabbageissmall@gmail.com>
2025-04-24 00:44:58 -0400

    basic movement
    Basic movement, working to use velocity to get a smoothened out movement.

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var speed = 500
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	velocity = velocity.normalized() * speed
+
+	self.velocity = velocity  # Use the built-in velocity property
+	move_and_slide()  # No arguments in Godot 4
