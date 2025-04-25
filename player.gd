7c0d939e77857c00c47a2aa193ab9b3c9e84a82e
hussain2x <hussain.ali.hus09@gmail.com>
2025-04-25 23:18:26 +0100

    added info chart, and working on sprinting
    AAAAAHHHH

--- player.gd
+++ player.gd
@@ -1,11 +1,17 @@
 extends CharacterBody2D
 
 var speed = 500
-
-func _physics_process(delta):
+var sprint = 0
+func _physics_process(_delta):
+	speed += sprint
+	if Input.is_action_pressed("sprint"):
+		sprint = 300
+	else:
+		sprint = 0
+	@warning_ignore("shadowed_variable_base_class")
 	var velocity = Vector2.ZERO
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = velocity.normalized() * speed
 	look_at(get_global_mouse_position())
 
