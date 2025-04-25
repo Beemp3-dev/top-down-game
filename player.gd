29b624548d32f850ed5f072994a8ff384f2be884
Beemp3 <thecabbageissmall@gmail.com>
2025-04-25 17:31:28 -0400

    Added player facing mouse
    Adds a single line of code.

--- player.gd
+++ player.gd
@@ -7,6 +7,7 @@ func _physics_process(delta):
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	velocity = velocity.normalized() * speed
+	look_at(get_global_mouse_position())
 
 	self.velocity = velocity  # Use the built-in velocity property
 	move_and_slide()  # No arguments in Godot 4
